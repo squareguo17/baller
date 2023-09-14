6e25fff06d225700a019a311dc40f80e79c90787
BoscoLeet <787534184@qq.com>
2023-09-14 17:09:59 +0800

    First Spring Boot App

---
+++ src/main/java/com/balldance/baller/BallerApplication.java
@@ -0,0 +1,13 @@
+package com.balldance.baller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balldance/baller/BallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balldance.baller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
